0a2eaa6954cd9428d14a00612d73150ec877cd4e
John Hollén <johnhollen982@gmail.com>
2014-12-12 16:53:28 +0100

    Progress

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,49 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.SphereGeometry(20, 100, 100);
-// create the sphere's material
-var shaderMaterial = new THREE.ShaderMaterial({
 
-  vertexShader:   $('#vertexshader').text(),
-  fragmentShader: $('#fragmentshader').text()
-});
-camera.position.z = 100;
+// create the sphere's material
+
+camera.position.z = 1000;
 camera.position.x = -100;
 camera.position.y = 100;
 
 
 var controls = new THREE.FirstPersonControls(camera);
 
-var sphere = new THREE.Mesh(geometry, shaderMaterial);
-scene.add(sphere);
+
+/******Things for sphere ***********/
+
+var shaderMaterial = new THREE.ShaderMaterial({
+
+  vertexShader:   $('#planetVertexShader').text(),
+  fragmentShader: $('#planetFragmentShader').text()
+});
+
+var geometry = [
+
+  [ new THREE.SphereGeometry(100, 200, 200), 50 ],
+  [ new THREE.SphereGeometry(100, 150, 150), 300 ],
+  [ new THREE.SphereGeometry(100, 100, 100), 1000 ],
+  [ new THREE.SphereGeometry(100, 50, 50), 2000 ],
+  [ new THREE.SphereGeometry(100, 25, 25), 8000 ]
+
+];
+
+lod = new THREE.LOD();
+
+for ( i = 0; i < geometry.length; i ++ ) {
+
+  mesh = new THREE.Mesh( geometry[ i ][ 0 ], shaderMaterial );
+  mesh.scale.set( 1.5, 1.5, 1.5 );
+  mesh.updateMatrix();
+  mesh.matrixAutoUpdate = false;
+  lod.addLevel( mesh, geometry[ i ][ 1 ] );
+  scene.add( lod );
+}
+
+//var sphere = new THREE.Mesh(geometry, shaderMaterial);
+//scene.add(sphere);
 
 
 var skyBoxShaderMaterial = new THREE.ShaderMaterial({
@@ -45,7 +72,16 @@ function animate() {
 }
 
 function render(){
+  scene.updateMatrixWorld();
+  scene.traverse( function ( object ) {
 
+    if ( object instanceof THREE.LOD ) {
+
+      object.update( camera );
+
+    }
+
+  } );
   renderer.render(scene, camera);
 }
 
